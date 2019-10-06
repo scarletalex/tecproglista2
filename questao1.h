3d2c7aaeb463eb954d5a376d14950bcc8ccd126b
scarletalex <55985690+scarletalex@users.noreply.github.com>
2019-10-06 10:56:11 -0300

    1

---
+++ questao1.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Aluno{
+    private:
+    string nome;
+    string disciplina[100];
+    float media;
+    float nota[100];
+    int contador;
+
+    public:
+    void coletaAluno();
+    void mostrarAluno();
+
+};
